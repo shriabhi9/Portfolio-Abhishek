c00ab600f53237c703629afc335f2229a82dc9d4
abhishek <shriabhishek929@gmail.com>
2024-05-23 16:20:24 +0530

    update in media query

--- src/Pages/Connects.jsx
+++ src/Pages/Connects.jsx
@@ -6,32 +6,32 @@ export const Connects = () => {
       <div className="w-[80%] flex items-start justify-start max-md:flex-wrap md:gap-10 gap-2">
         {/* left section */}
         <div className="md:w-[70%] w-[240px] duration-500 transition-all">
-          <h1 className="text-white md:text-[7rem] text-[2rem] mb-4 duration-500 transition-all">
+          <h1 className="text-white md:text-[3rem] text-[1.7rem] mb-4 duration-500 transition-all">
             Let's start a project together
           </h1>
           <form className="flex flex-col gap-8 pt-[4rem] mt-4 mb-4">
             <label className="flex flex-col items-start gap-4">
-              <p className="text-white md:text-[3rem] text-[1.5rem] duration-500 transition-all">What's your name?</p>
+              <p className="text-white md:text-[2rem] text-[1rem] duration-500 transition-all">What's your name?</p>
               <input
-                className="text-white bg-transparent outline-none md:text-[3rem] text-[1.5rem] mx:text-[8px] duration-500 transition-all"
+                className="text-white bg-transparent outline-none md:text-[2rem] text-[1rem] mx:text-[8px] duration-500 transition-all"
                 type="text"
                 placeholder="John Doe"
               />
             </label>
             <div className="w-full h-[2px] second-color mt-4 mb-4"></div>
             <label className="flex flex-col items-start gap-4">
-              <p className="text-white md:text-[3rem] text-[1.5rem] duration-500 transition-all">What's your email?</p>
+              <p className="text-white md:text-[2rem] text-[1rem] duration-500 transition-all">What's your email?</p>
               <input
-                className="text-white bg-transparent outline-none md:text-[3rem] text-[1.5rem] duration-500 transition-all"
+                className="text-white bg-transparent outline-none md:text-[2rem] text-[1rem] duration-500 transition-all"
                 type="text"
                 placeholder="john@gmail.com"
               />
             </label>
             <div className="w-full h-[2px] second-color mt-4 mb-4"></div>
             <label className="flex flex-col items-start gap-4">
-              <p className="text-white md:text-[3rem] text-[1.5rem] duration-500 transition-all">Your message</p>
+              <p className="text-white md:text-[2rem] text-[1rem] duration-500 transition-all">Your message</p>
               <input
-                className="text-white bg-transparent outline-none md:text-[3rem] text-[1.5rem] duration-500 transition-all"
+                className="text-white bg-transparent outline-none md:text-[2rem] text-[1rem] duration-500 transition-all"
                 type="text"
                 placeholder="Hi Abhishek, can we..."
               />
@@ -53,20 +53,20 @@ export const Connects = () => {
             <GoArrowDownRight className="text-white text-[40px]" />
           </div>
           <div className="flex flex-col items-start gap-2">
-            <p className="font-bold text-[#5c626d]">CONTACT DETAILS</p>
+            <p className="font-bold text-[#5c626d] md:text-[1rem] text-xs">CONTACT DETAILS</p>
             <button className="flex flex-col items-center transition-all duration-200 hover:scale-95 group gap-1 text-white">
               <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBpDltpvjQdpndTLLmcrsxpsXNNdJDSfWvDXkMjcNrvlhbzwRFDLWWjPdfQjVWVpVlwHBzg">
-                <p className="md:text-xl text-md">shrivastavaabhishek929@gmail.com</p>
+                <p className="md:text-[1rem] text-[13px]">shrivastavaabhishek929@gmail.com</p>
               </a>
               <div className="w-[calc(100%-4px)] h-[1px] bg-white scale-0 group-hover:scale-100 transition-all duration-200" />
             </button>
             <button className="flex flex-col items-center justify-start transition-all duration-200 hover:scale-95 group gap-1 text-white">
-              <p className="md:text-xl text-md">+917024744625</p>
+              <p className="md:text-[1rem] text-xs">+917024744625</p>
               <div className="w-[calc(100%-4px)] h-[1px] bg-white scale-0 group-hover:scale-100 transition-all duration-200" />
             </button>
           </div>
           <div className="flex flex-col items-start gap-2">
-            <p className=" font-bold text-[#5c626d] ">SOCIALS</p>
+            <p className=" font-bold text-[#5c626d] md:text-[1rem] text-xs">SOCIALS</p>
             <button
               className="flex flex-col items-center transition-all duration-200 hover:scale-95 group gap-1 text-white"
               onClick={() => {
@@ -76,7 +76,7 @@ export const Connects = () => {
                 );
               }}
             >
-              <p className="md:text-xl text-md">LinkedIn</p>
+              <p className="md:text-[1rem] text-xs">LinkedIn</p>
               <div className="w-[calc(100%-4px)] h-[1px] bg-white scale-0 group-hover:scale-100 transition-all duration-200" />
             </button>
             <button
@@ -85,7 +85,7 @@ export const Connects = () => {
                 window.open("https://twitter.com/___abhishekk", "_blank");
               }}
             >
-              <p className="md:text-xl text-md">Twitter</p>
+              <p className="md:text-[1rem] text-xs">Twitter</p>
 
               <div className="w-[calc(100%-4px)] h-[1px] bg-white scale-0 group-hover:scale-100 transition-all duration-200" />
             </button>
@@ -95,7 +95,7 @@ export const Connects = () => {
                 window.open("https://github.com/shriabhi9", "_blank");
               }}
             >
-              <p className="md:text-xl text-md">Github</p>
+              <p className="md:text-[1rem] text-xs">Github</p>
               <div className="w-[calc(100%-4px)] h-[1px] bg-white scale-0 group-hover:scale-100 transition-all duration-200" />
             </button>
           </div>
